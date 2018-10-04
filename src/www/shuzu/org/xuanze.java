a5326e06ed167fb7d666ab671378bc6c4ce5d4a9
showgegui <923897576@qq.com>
2018-10-04 16:01:12 +0800

    2

--- src/www/shuzu/org/xuanze.java
+++ src/www/shuzu/org/xuanze.java
@@ -6,18 +6,22 @@ public class xuanze {
 
 	public static void main(String[] args) {
 		int a[]=new int[10];
-		//将输入的十个数，依次赋值给数组
+		int b[]=new int[10];
+		
+//		依次给数组赋值
 		for(int i=0;i<10;i++){
 		Scanner sc=new Scanner(System.in);
+		Scanner sb=new Scanner(System.in);
+		b[i]=sb.nextInt();
+		
 		System.out.println("please input the "+(i+1)+" number");
 		a[i]=sc.nextInt();
 		}
-		//两层循环，进行冒泡排序 
+//		冒泡排序
 		for(int j=0;j<a.length;j++){
 		for(int k=0;k<a.length-j-1;k++){
 		int l;
 		if(a[k]>a[k+1]){
-
 		l=a[k];
 		a[k]=a[k+1];
 		a[k+1]=l;
